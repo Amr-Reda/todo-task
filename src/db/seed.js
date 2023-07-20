140ad6232edf6cc0dfca62f5f2e77cd7d9a93065
Amr-Reda <amrreda547@gmail.com>
2023-07-20 19:44:29 +0300

    update seed data

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,22 +1,29 @@
 const User = require('../modules/user/model');
+const Item = require('../modules/item/model');
 
 const initSeed = async () => {
     const users = await User.countDocuments()
 
     if (users != 0) return
 
-	// import users
-    const user = {
+	// import data
+    const userData = {
         password: 'Password123#',
         email: 'super.user@mail.com',
         name: 'super user',
     }
-	await User.create(user)
+	const user = await User.create(userData)
+    const itemData = {
+        task: 'task 1',
+        description: 'description 1',
+        userId: user._id,
+    }
+	await Item.create(itemData)
 
     console.log("Seed Loaded Successfully");
     console.log("User Data ->");
-    console.log("email: ", user.email);
-    console.log("password: ", user.password);
+    console.log("email: ", userData.email);
+    console.log("password: ", userData.password);
     return
 }
 
